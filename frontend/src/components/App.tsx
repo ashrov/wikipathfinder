82faf5e7f74607b76750447d518a96dd7f1b35b5
Govnocode4U <konstantinkozoriz1310@gmail.com>
2023-10-28 18:26:43 +0300

    add to App.tsx getPathsFromApi that use getPaths.ts

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import '../styles/App.css';
+import {InputFields} from "src/components/InputFields";
+import {getPathsResponse, getUsers, requestBody} from "src/ts/getPaths";
+import {Result} from "src/components/Result";
+
+function App() {
+    const [start, setStart] = useState('');
+    const [end, setEnd] = useState('');
+    const [error, setError] = useState('');
+    const [paths, setPaths] = useState<string | getPathsResponse>()
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const request: requestBody = {
+        namespace: 'ru',
+        start: start,
+        end: end
+    };
+
+    function getPathsFromApi() {
+        //set loading
+        setIsLoading(true)
+
+        //send request
+        getUsers(request)
+            .then((response) => {
+                //response from API
+                setPaths(response);
+                console.log(typeof response)
+            })
+            .catch((error) => {
+                //Error from API
+                setError(error.message)
+            });
+    }
+
+    return (
+        <div className={"App"}>
+            <InputFields
+                start={start}
+                end={end}
+                setStart={setStart}
+                setEnd={setEnd}
+                onClickButton={getPathsFromApi}
+            />
+            <Result paths={paths}/>
+        </div>
+    );
+}
+
+export default App;
