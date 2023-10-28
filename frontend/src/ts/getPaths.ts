a8d4efe497b218ef2cf4bede0b7214e04d2432e1
Govnocode4U <konstantinkozoriz1310@gmail.com>
2023-10-28 18:22:32 +0300

    added getPaths.ts: script for GET-request

---
+++ frontend/src/ts/getPaths.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export interface path {
+    link: string
+}
+
+export interface getPathsResponse {
+    data: path[];
+}
+
+export interface requestBody {
+    namespace: 'ru'
+    start: string,
+    end: string
+}
+
+export async function getUsers(request: requestBody) {
+    try {
+        const {data} = await axios.get<getPathsResponse>(
+            'http://localhost:45678/path_by_names',
+            {
+                params: {
+                    namespace: request.namespace,
+                    start: request.start,
+                    end: request.end
+                },
+                headers: {
+                    Accept: 'application/json',
+                },
+            },
+        );
+
+        console.log(JSON.stringify(data, null, 4));
+
+        return data;
+
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            return error.message;
+        } else {
+            return 'An unexpected error occurred';
+        }
+    }
+}
+
