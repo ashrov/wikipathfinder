07828b501c117f9abdf3f0c07afb1d16c0fa05a4
Govnocode4U <konstantinkozoriz1310@gmail.com>
2023-10-30 19:51:45 +0300

    test-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,38 +4,41 @@ module.exports = {
         es2021: true,
         jest: true,
     },
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
         },
         ecmaVersion: "latest",
-        sourceType: 'module',
+        sourceType: "module",
     },
-    plugins: ['react', '@typescript-eslint', 'security'],
+    // plugins: ["react", "@typescript-eslint", "security"],
     extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'plugin:security/recommended',
-        'plugin:import/typescript',
-        'prettier',
+        "plugin:react/recommended",
+        "airbnb",
+        "plugin:security/recommended",
+        "plugin:import/typescript",
+        "prettier",
     ],
 
     rules: {
-        'import/extensions': [
-            'error',
-            'ignorePackages',
+        "import/extensions": [
+            "error",
+            "ignorePackages",
             {
-                ts: 'never',
-                tsx: 'never',
+                ts: "never",
+                tsx: "never",
             },
         ],
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': 'error',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['warn'],
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-filename-extension': ['error', {extensions: ['.tsx']}],
-        'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": "error",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["warn"],
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+        "import/no-extraneous-dependencies": [
+            "error",
+            { devDependencies: true },
+        ],
     },
-}
+};
